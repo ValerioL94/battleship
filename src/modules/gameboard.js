1328263fc3c8cc75ab7265f47a1c276e93aa5dd3
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-03-01 16:21:21 +0100

    Remove redundant code

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -2,7 +2,6 @@ import Ship from './ship.js';
 export default class Gameboard {
   constructor() {
     this.board = new Array(10).fill(null).map(() => new Array(10).fill(null));
-    this.missedShots = [];
     this.ships = [];
   }
 
@@ -44,35 +43,18 @@ export default class Gameboard {
     }
   }
 
-  isValid(cell) {
-    if (cell[0] < 0 || cell[1] < 0 || cell[0] > 9 || cell[1] > 9) return false;
-    else return true;
-  }
-
-  isEmpty(cell) {
-    if (cell(0) != null || cell[1] != null) return false;
-    else return true;
-  }
-
   receiveAttack(coords) {
     if (this.board[coords[0]][coords[1]] === null) {
-      this.board[coords[0]][coords[1]] = 'miss';
-      this.missedShots.push(coords);
       return false;
-    } else if (
-      this.board[coords[0]][coords[1]] !== null &&
-      this.board[coords[0]][coords[1]] !== 'miss'
-    ) {
+    } else {
       this.board[coords[0]][coords[1]].hit();
       return true;
     }
   }
 
   isGameOver() {
-    for (let i = 0; i < this.ships.length; i++) {
-      if (!this.ships[i].isSunk()) {
-        return false;
-      }
+    for (let i = 0; i < 5; i++) {
+      if (!this.ships[i].isSunk()) return false;
     }
     return true;
   }
--- src/modules/ship.js
+++ src/modules/ship.js
@@ -13,9 +13,3 @@ export default class Ship {
     else return false;
   }
 }
-
-// const carrier = new Ship(5);
-// const battleship = new Ship(4);
-// const cruiser = new Ship(3);
-// const submarine = new Ship(3);
-// const destroyer = new Ship(2);
