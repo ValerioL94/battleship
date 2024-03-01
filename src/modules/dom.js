10dc1c5af3faa8595bc8e2d9405382c6a86fdda1
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-03-01 20:57:56 +0100

    Add drag and drop listeners

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,11 +1,64 @@
+import gameLoop from './game-loop.js';
 const dom = (() => {
   const player = document.querySelector('#player > h2');
   const opponent = document.querySelector('#opponent > h2');
-  function showPlayerName(name) {
-    player.textContent = name;
+  const start = document.getElementById('start');
+  const confirmBtn = document.getElementById('confirm');
+  const playerName = document.getElementById('playerName');
+  const playerBoard = document.getElementById('playerBoard');
+  const content = document.getElementById('content');
+  function newGame() {
+    start.showModal();
   }
-  function showOpponentName(name) {
-    opponent.textContent = name;
+  confirmBtn.addEventListener('click', () => {
+    start.close();
+    gameLoop.initPlayers(playerName.value);
+    showNames(`Captain ${playerName.value}`);
+    gameLoop.initBoards();
+    content.classList.remove('hide');
+  });
+
+  playerBoard.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  });
+  playerBoard.addEventListener('drop', (event) => {
+    event.preventDefault();
+    if (event.target.className === 'cell') {
+      event.target.style.border = '';
+      const data = event.dataTransfer.getData('text');
+      event.target.appendChild(document.getElementById(data));
+    }
+  });
+  playerBoard.addEventListener('dragenter', function (event) {
+    if (event.target.className === 'cell') {
+      event.target.style.border = '3px dotted red';
+    }
+  });
+  playerBoard.addEventListener('dragleave', function (event) {
+    if (event.target.className === 'cell') {
+      event.target.style.border = '';
+    }
+  });
+  // const carrier = document.getElementById('carrier');
+  // const battleship = document.getElementById('battleship');
+  // const cruiser = document.getElementById('cruiser');
+  // const submarine = document.getElementById('submarine');
+  // const destroyer = document.getElementById('destroyer');
+  // carrier,
+  // battleship,
+  // cruiser,
+  // submarine,
+  // destroyer
+  const ships = document.getElementById('shipsContainer');
+  ships.addEventListener('dragstart', (event) => {
+    if (event.target.className === 'ship') {
+      event.dataTransfer.setData('text', event.target.id);
+    }
+  });
+
+  function showNames(p1, p2 = 'Captain AI') {
+    player.textContent = p1;
+    opponent.textContent = p2;
   }
   function fillCell(ship, coords) {
     if (ship === 'carrier') {
@@ -64,8 +117,7 @@ const dom = (() => {
   }
 
   return {
-    showPlayerName,
-    showOpponentName,
+    newGame,
     fillCell,
     displayShot,
   };
